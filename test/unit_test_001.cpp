799237017f087f41a87e1e88e1356209962decb3
rob tillaart <rob.tillaart@gmail.com>
2021-01-16 19:57:41 +0100

    fix unit test

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -31,14 +31,8 @@
 
 #include <ArduinoUnitTests.h>
 
-#define assertEqualFloat(arg1, arg2, arg3)  assertOp("assertEqualFloat", "expected", fabs(arg1 - arg2), compareLessOrEqual, "<=", "actual", arg3)
-// #define assertEqualINF(arg)  assertOp("assertEqualINF", "expected", INFINITY, compareEqual, "==", "actual", arg)
-// #define assertEqualNAN(arg)  assertOp("assertEqualNAN", "expected", true, compareEqual, "==", "actual", isnan(arg))
-
-
 #include "Arduino.h"
 #include "MS5611.h"
-
 
 
 unittest_setup()
